7ceb3b0a19bf83788343226daa521bd17d473a6d
tetradog <tetradog@gmail.com>
2016-02-18 21:28:31 +0100

    subir clases

---
+++ Gabriel.Cat.MatGrafics/ElementoPatron.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabriel.Cat.MatGrafics
+{
+    /// <summary>
+    /// Es una clase que repesenta un elemento de la clase patron
+    /// </summary>
+    public class ElementoPatron
+    {
+
+    }
+}
