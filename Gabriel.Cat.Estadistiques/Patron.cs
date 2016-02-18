7fc765df547ae0dc4089a9ce1b44e28cde1df0e8
tetradog <tetradog@gmail.com>
2016-02-19 00:06:20 +0100

    subir clases

---
+++ Gabriel.Cat.Estadistiques/Patron.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gabriel.Cat.MatGrafics
+{
+    /// <summary>
+    /// es una clase para crear patrones de busqueda
+    /// </summary>
+    public class Patron
+    {
+    }
+}
